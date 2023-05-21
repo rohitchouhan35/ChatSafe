6d870ccce7866316ae462c8e70cde19d3012fcb3
rohit <rohitchouhan3512@gmail.com>
2023-05-21 15:41:06 +0530

    db connection needed

---
+++ chat.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chatSafe</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="chat-area">
+            <header>
+                <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+                <img src="img/rohit.jpg" alt="">
+                <div class="details">
+                    <span>Rohit</span>
+                    <p>Active now</p>
+                </div>
+            </header>
+            <div class="chat-box">
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Hello! are you coming to college today?</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="#" alt="">
+                    <div class="details">
+                        <p>Yupp! and you?</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Hello! are you coming to college today?</p>
+                    </div>
+                </div>
+                <div class="chat incoming">
+                    <img src="#" alt="">
+                    <div class="details">
+                        <p>Yupp! and you?</p>
+                    </div>
+                </div>
+                <div class="chat outgoing">
+                    <div class="details">
+                        <p>Hello! are you coming to college today?</p>
+                    </div>
+                </div>
+            </div>
+            <form action="#" class="typing-area">
+                <input type="text" placeholder="Message">
+                <button><i class="fab fa-telegram-plane"></i></button>
+            </form>
+        </section>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chatSafe</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form login">
+            <header>Chat Safe</header>
+            <form action="#">
+                <div class="error-text">This is an error message!</div>
+                <div class="field input">
+                    <label>Email Address</label>
+                    <input type="text" placeholder="Enter you email">
+                </div>
+                <div class="field input">
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter your password">
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field button">
+                    <input type="submit" value="Continue to Chat">
+                </div>
+            </form>
+            <div class="link">Not yet signed up? <a href="#">Signup now</a></div>
+        </section>
+    </div>
+    <script src="javascript/pass-show-hide.js"></script>
+</body>
+</html>
