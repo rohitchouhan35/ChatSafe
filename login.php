567235675cce89d948416cc39962a886713c70a5
rohit <rohitchouhan3512@gmail.com>
2023-05-25 23:29:54 +0530

    now user is logged in across diffrent tabs

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(isset($_SESSION['unique_id'])) { // if logged in
+        header("location: users.php");
+    }
+?>
+
 <?php  include_once "header.php"; ?>
 <body>
     <div class="wrapper">
